ef99d79325474ac03ebf02d398a674867a8b1316
B. Travis Howe <b.travis.howe@gmail.com>
2016-11-18 08:04:37 -0700

    dashatize revised

--- Dashatize it.py
+++ Dashatize it.py
@@ -1,9 +1,11 @@
 import re
 def dashatize(num):
-    dash = re.sub(r'([13579])', r'-\1-', str(num))
-    dash = re.sub(r'--', r'-', dash)
-    return dash.strip('-')
+    # dash = re.sub(r'([13579])', r'-\1-', str(num))
+    # dash = re.sub(r'--', r'-', dash)
+    # return dash.strip('-')
+
+    return re.sub(r'([13579])', r'-\1-', str(num)).replace('--', '-').strip('-')
 
 
 if __name__=="__main__":
-    print dashatize(-28369)
+    print dashatize(None)
