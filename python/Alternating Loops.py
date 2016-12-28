db44bbd0513d66152e08ecfc7d4a30a615f84718
B. Travis Howe <b.travis.howe@gmail.com>
2016-12-27 21:07:44 -0700

    alternating Loops

--- python/Alternating Loops.py
+++ python/Alternating Loops.py
@@ -1,16 +1,26 @@
 def combine(*args):
-    args = list(args)
+    # args2 = list(args+tuple())
+    # hi = args+tuple()
+    #
+    # lst = []
+    # i = 0
+    # while len([item for l in args2 for item in l])>0:
+    #     ind = (i)%len(args2)
+    #     if len(args2[ind])>0:
+    #         lst.append(args2[ind].pop(0))
+    #     i+=1
+    # return lst
 
-    lst = []
-    i = 0
-    while len([item for l in args for item in l])>0:
-        ind = (i)%len(args)
-        if len(args[ind])>0:
-            lst.append(args[ind].pop(0))
-        i+=1
-    return lst
+    # a = []
+    # max_l = max([len(lst) for lst in args])
+    # for ind in xrange(max_l):
+    #     for lst in list(args):
+    #         if len(lst)>ind:
+    #             a.append(lst[ind])
+    # return a
 
-
+    max_l = max([len(lst) for lst in args])
+    return [lst[ind] for ind in xrange(max_l) for lst in list(args) if len(lst)>ind]
 
 
 
@@ -18,4 +28,4 @@ def combine(*args):
 
 
 if __name__=="__main__":
-    combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8])
+    print combine(['a', 'b', 'c'], [1, 2, 3, 4, 5], [6, 7], [8])
