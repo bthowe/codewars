7ae35a0f6582820b55085531f406ebe3d944a646
B. Travis Howe <b.travis.howe@gmail.com>
2016-12-29 08:57:31 -0700

    Sum of pairs draft...optimize

--- python/Sum of Pairs.py
+++ python/Sum of Pairs.py
@@ -1,19 +1,17 @@
 import numpy as np
 def sum_pairs(lst, sum):
-    lst = np.array(lst)
-    for ind, l in enumerate(lst):
-        lst_sum = l + lst
-        for i in xrange(len(lst_sum)):
-            if lst_sum[i]==sum and i!=ind:
-                return [l, lst[i]]
-
-
-
-
-
-
-
-
+    index_sum = -1
+    nums = []
+    for ind, l in enumerate(lst[:-1]):
+        lst_sum = l + np.array(lst[ind+1:])
+        index = np.where(lst_sum[::-1]==sum)[0]
+        if len(index)>0:
+            if index_sum<int(index):
+                index_sum = int(index)
+                nums = [l, lst[-(index_sum+1)]]
+    if nums:
+        return nums
+    return None
 
 if __name__=="__main__":
-    print sum_pairs([10, 5, 2, 3, 7, 5], 10)
+    print sum_pairs([10, 5, 2, 3, 7, 5, 0], 7)
