122ba2009507559889437cef0ee77ca7ebffc153
B. Travis Howe <b.travis.howe@gmail.com>
2017-01-10 18:39:46 -0700

    Sum of pairs draft

--- python/Sum of Pairs.py
+++ python/Sum of Pairs.py
@@ -1,17 +1,32 @@
-import numpy as np
+# import numpy as np
+# def sum_pairs(lst, sum):
+#     index_sum = -1
+#     nums = []
+#     for ind, l in enumerate(lst[:-1]):
+#         lst_sum = l + np.array(lst[ind+1:])
+#         print lst_sum
+#         index = np.where(lst_sum[::-1]==sum)[0]
+#         if len(index)>0:
+#             if index_sum<int(index):
+#                 index_sum = int(index)
+#                 nums = [l, lst[-(index_sum+1)]]
+#     if nums:
+#         return nums
+#     return None
+
 def sum_pairs(lst, sum):
-    index_sum = -1
     nums = []
-    for ind, l in enumerate(lst[:-1]):
-        lst_sum = l + np.array(lst[ind+1:])
-        index = np.where(lst_sum[::-1]==sum)[0]
-        if len(index)>0:
-            if index_sum<int(index):
-                index_sum = int(index)
-                nums = [l, lst[-(index_sum+1)]]
+    index_sum = 10**10
+    for ind1, addend1 in enumerate(lst[:-1]):
+        for ind2, addend2 in enumerate(lst[ind1:]):
+            if addend1 + addend2 == sum:
+                if ind1+ind2+1<index_sum:
+                    index_sum = ind1+ind2+1
+                    nums = [addend1, addend2]
     if nums:
         return nums
     return None
 
+
 if __name__=="__main__":
-    print sum_pairs([10, 5, 2, 3, 7, 5, 0], 7)
+    print sum_pairs([10, 5, 1, 0, 2, 7, 2, 9], 7)
