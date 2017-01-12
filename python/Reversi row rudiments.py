ce0ba5458c5092876384953297ed20c31af93588
B. Travis Howe <b.travis.howe@gmail.com>
2017-01-11 20:58:24 -0700

    Sum of pairs still too slow...likely will be forever

--- python/Reversi row rudiments.py
+++ python/Reversi row rudiments.py
@@ -34,33 +34,4 @@ def reversi_row(row):
 
 
 if __name__=="__main__":
-    # rev = Reversi([0,1,7,3,2])
-    # print rev.play()
-    # #not sure about this sequence
-
-
-    # print reversi_row([5, 1, 3, 4, 6])
     print reversi_row([3, 4])
-
-
-
-# go = True
-# while go:
-#     go = False
-#     for i in xrange(1, 7):
-#         if (self.board[i-1]==self.board[i+1]) and (self.board[i-1]!=self.board[i]) and (self.board[i-1]!='.') and (self.board[i]!='.'):
-#             self.board[i] = self.players[self.turn]
-#             go = True
-
-# t1 = self.turn
-# t2 = (self.turn+1)%2
-# for i in xrange(2):
-#     regex = re.escape(self.players[t1]) + r'[' + re.escape(self.players[t2]) + r']+' + re.escape(self.players[t1])
-#     results = True
-#     while results:
-#         results = re.findall(regex, ''.join(self.board))
-#         board_string = ''.join(self.board)
-#         if results:
-#             self.board = [i for i in ''.join(self.board).replace(results[0], results[0][0]*len(results[0]))]
-#     t1 = t2
-#     t2 = (t2+1)%2
--- python/Sum of Pairs.py
+++ python/Sum of Pairs.py
@@ -1,32 +1,38 @@
-# import numpy as np
-# def sum_pairs(lst, sum):
-#     index_sum = -1
-#     nums = []
-#     for ind, l in enumerate(lst[:-1]):
-#         lst_sum = l + np.array(lst[ind+1:])
-#         print lst_sum
-#         index = np.where(lst_sum[::-1]==sum)[0]
-#         if len(index)>0:
-#             if index_sum<int(index):
-#                 index_sum = int(index)
-#                 nums = [l, lst[-(index_sum+1)]]
-#     if nums:
-#         return nums
-#     return None
+import numpy as np
 
 def sum_pairs(lst, sum):
-    nums = []
-    index_sum = 10**10
-    for ind1, addend1 in enumerate(lst[:-1]):
-        for ind2, addend2 in enumerate(lst[ind1:]):
-            if addend1 + addend2 == sum:
-                if ind1+ind2+1<index_sum:
-                    index_sum = ind1+ind2+1
-                    nums = [addend1, addend2]
-    if nums:
-        return nums
+    # nums = []
+    # index_sum = 10**10
+    # for ind1, addend1 in enumerate(lst[:-1]):
+    #     for ind2, addend2 in enumerate(lst[ind1+1:]):
+    #         if addend1 + addend2 == sum:
+    #             if ind1+ind2+1<index_sum:
+    #                 index_sum = ind1+ind2+1
+    #                 nums = [addend1, addend2]
+    # if nums:
+    #     return nums
+    # return None
+
+
+# perhaps a faster way...still times out
+    add_mat = np.array([lst]).T + np.array([lst]*len(lst))
+
+    add_triu = np.triu(add_mat, 1) - sum*np.tri(len(lst))
+
+    mask = np.where(add_triu==sum)
+    if len(mask[0])>0:
+        index = min(zip(mask[0], mask[1]), key = lambda x: max(x))
+
+        return [lst[index[0]], lst[index[1]]]
     return None
 
 
+
+
+
+
+
+
+
 if __name__=="__main__":
-    print sum_pairs([10, 5, 1, 0, 2, 7, 2, 9], 7)
+    print sum_pairs([1, 4, 8, 7, 3, 15], 8)
